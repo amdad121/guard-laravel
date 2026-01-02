16bf6c56ed599dc897392e8454467be0c30e7ca7
Amdadul Haq <amdadulhaq781@gmail.com>
2026-01-03 01:40:28 +0600

    build: update dev dependencies and clean test setup
    - Relax laravel/pint version constraint
    - Remove redundant setUp method from TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,11 +11,6 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-    }
-
     protected function getPackageProviders($app)
     {
         return [
